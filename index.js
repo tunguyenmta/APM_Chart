40617607d9d11af07a9d2ea911c17ac41364a0c8
TuNN <nguyentu72146@gmail.com>
2024-04-26 09:16:10 +0700

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname));
 const server = http.createServer(app);
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 const args = process.argv.slice(2)
